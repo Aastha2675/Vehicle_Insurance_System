4ea5b54e6993b3a260c04228cbd74688a23ab635
Aastha2675 <aasthamhatre2607@gmail.com>
2026-03-19 16:24:22 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = "src",
+  version="0.0.1",
+  author="Aastha Mhatre",
+  author_email = "aasthamhatre2607@gmail.com",
+  packages = find_packages()
+)
